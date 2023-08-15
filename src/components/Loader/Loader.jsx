127e26096ef68c0df8064f27c5c5726fe83afc41
SkyMolfar <skymolfars@gmail.com>
2023-08-15 09:44:49 +0200

    json

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Watch } from 'react-loader-spinner';
 
 export function Loader() {
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []); 
+  
   return (
-    <Watch
-      height="80"
-      width="80"
-      radius="48"
-      color="#3f51b5"
-      ariaLabel="watch-loading"
-      wrapperStyle={{
+    <div
+      style={{
         position: 'absolute',
         top: '50%',
         left: '50%',
-        transform: 'translate(-50%, -50%)', 
+        transform: 'translate(-50%, -50%)',
         visibility: isLoading ? 'visible' : 'hidden',
       }}
-    />
+    >
+      <Watch
+        height={80}
+        width={80}
+        radius={48}
+        color="#3f51b5"
+        ariaLabel="watch-loading"
+      />
+    </div>
   );
 }
-
